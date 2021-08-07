eab75039a7415a21ffc25df1468ab4d63306598e
Benedikt Full <74531112+fullben@users.noreply.github.com>
2021-08-07 15:58:42 +0200

    Add some more functionality to JPA repos

--- server/src/main/java/de/uniba/dsg/jpb/server/data/access/jpa/CustomerRepository.java
+++ server/src/main/java/de/uniba/dsg/jpb/server/data/access/jpa/CustomerRepository.java
@@ -2,9 +2,12 @@ package de.uniba.dsg.jpb.server.data.access.jpa;
 
 import de.uniba.dsg.jpb.data.model.jpa.CustomerEntity;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
 
   List<CustomerEntity> findByDistrictId(Long districtId);
+
+  Optional<CustomerEntity> findByEmail(String email);
 }
--- server/src/main/java/de/uniba/dsg/jpb/server/data/access/jpa/OrderRepository.java
+++ server/src/main/java/de/uniba/dsg/jpb/server/data/access/jpa/OrderRepository.java
@@ -2,9 +2,29 @@ package de.uniba.dsg.jpb.server.data.access.jpa;
 
 import de.uniba.dsg.jpb.data.model.jpa.OrderEntity;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
 
   List<OrderEntity> findByDistrictId(Long districtId);
+
+  @Query(
+      value =
+          "SELECT * FROM OrderEntity o WHERE o.customer_id = :customerId ORDER BY o.entry_date DESC LIMIT 1",
+      nativeQuery = true)
+  Optional<OrderEntity> findMostRecentOrderOfCustomer(Long customerId);
+
+  @Query(
+      value =
+          "SELECT * FROM OrderEntity o WHERE o.district_id = :districtId AND o.fulfilled = false ORDER BY entry_date ASC LIMIT 1",
+      nativeQuery = true)
+  Optional<OrderEntity> findOldestUnfulfilledOrderOfDistrict(Long districtId);
+
+  @Query(
+      value =
+          "SELECT * FROM OrderEntity o WHERE o.district_id = :districtId ORDER BY entry_date DESC LIMIT 20",
+      nativeQuery = true)
+  List<OrderEntity> find20MostRecentOrdersOfDistrict(Long districtId);
 }
