ac1de14902157716d1f13a4f1c26a8407ab6913a
Benedikt Full <74531112+fullben@users.noreply.github.com>
2021-08-07 16:02:25 +0200

    Allow access to new transactions in API

--- server/src/main/java/de/uniba/dsg/jpb/server/api/TransactionsController.java
+++ server/src/main/java/de/uniba/dsg/jpb/server/api/TransactionsController.java
@@ -1,11 +1,20 @@
 package de.uniba.dsg.jpb.server.api;
 
+import de.uniba.dsg.jpb.messages.DeliveryRequest;
+import de.uniba.dsg.jpb.messages.DeliveryResponse;
 import de.uniba.dsg.jpb.messages.OrderRequest;
 import de.uniba.dsg.jpb.messages.OrderResponse;
+import de.uniba.dsg.jpb.messages.OrderStatusRequest;
+import de.uniba.dsg.jpb.messages.OrderStatusResponse;
 import de.uniba.dsg.jpb.messages.PaymentRequest;
 import de.uniba.dsg.jpb.messages.PaymentResponse;
+import de.uniba.dsg.jpb.messages.StockLevelRequest;
+import de.uniba.dsg.jpb.messages.StockLevelResponse;
+import de.uniba.dsg.jpb.server.service.jpa.JpaDeliveryService;
 import de.uniba.dsg.jpb.server.service.jpa.JpaNewOrderService;
+import de.uniba.dsg.jpb.server.service.jpa.JpaOrderStatusService;
 import de.uniba.dsg.jpb.server.service.jpa.JpaPaymentService;
+import de.uniba.dsg.jpb.server.service.jpa.JpaStockLevelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,21 +28,48 @@ public class TransactionsController {
 
   private final JpaNewOrderService jpaNewOrderService;
   private final JpaPaymentService jpaPaymentService;
+  private final JpaOrderStatusService jpaOrderStatusService;
+  private final JpaDeliveryService jpaDeliveryService;
+  private final JpaStockLevelService jpaStockLevelService;
 
   @Autowired
   public TransactionsController(
-      JpaNewOrderService jpaNewOrderService, JpaPaymentService jpaPaymentService) {
+      JpaNewOrderService jpaNewOrderService,
+      JpaPaymentService jpaPaymentService,
+      JpaOrderStatusService jpaOrderStatusService,
+      JpaDeliveryService jpaDeliveryService,
+      JpaStockLevelService jpaStockLevelService) {
     this.jpaNewOrderService = jpaNewOrderService;
     this.jpaPaymentService = jpaPaymentService;
+    this.jpaOrderStatusService = jpaOrderStatusService;
+    this.jpaDeliveryService = jpaDeliveryService;
+    this.jpaStockLevelService = jpaStockLevelService;
   }
 
   @PostMapping(value = "transactions/new-order", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-  public OrderResponse newOrderTransaction(@RequestBody OrderRequest req) {
+  public OrderResponse doNewOrderTransaction(@RequestBody OrderRequest req) {
     return jpaNewOrderService.process(req);
   }
 
   @PostMapping(value = "transactions/payment", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-  public PaymentResponse newOrderTransaction(@RequestBody PaymentRequest req) {
+  public PaymentResponse doPaymentTransaction(@RequestBody PaymentRequest req) {
     return jpaPaymentService.process(req);
+  }
+
+  @PostMapping(
+      value = "transactions/order-status",
+      produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public OrderStatusResponse doOrderStatusTransaction(@RequestBody OrderStatusRequest req) {
+    return jpaOrderStatusService.process(req);
+  }
+
+  @PostMapping(value = "transactions/delivery", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public DeliveryResponse doDeliveryTransaction(@RequestBody DeliveryRequest req) {
+    return jpaDeliveryService.process(req);
+  }
+
+  @PostMapping(value = "transactions/stock-level", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public StockLevelResponse doStockLevelTransaction(@RequestBody StockLevelRequest req) {
+    return jpaStockLevelService.process(req);
   }
 }
