a8c2a95f41466218c172d2b4657286d7ec5beec0
Benedikt Full <74531112+fullben@users.noreply.github.com>
2021-08-11 16:15:50 +0200

    Fix some native queries

--- server/src/main/java/de/uniba/dsg/jpb/server/data/access/jpa/OrderRepository.java
+++ server/src/main/java/de/uniba/dsg/jpb/server/data/access/jpa/OrderRepository.java
@@ -12,19 +12,19 @@ public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
 
   @Query(
       value =
-          "SELECT * FROM OrderEntity o WHERE o.customer_id = :customerId ORDER BY o.entry_date DESC LIMIT 1",
+          "SELECT * FROM oorder o WHERE o.customer_id = :customerId ORDER BY o.entrydate DESC LIMIT 1",
       nativeQuery = true)
   Optional<OrderEntity> findMostRecentOrderOfCustomer(Long customerId);
 
   @Query(
       value =
-          "SELECT * FROM OrderEntity o WHERE o.district_id = :districtId AND o.fulfilled = false ORDER BY entry_date ASC LIMIT 1",
+          "SELECT * FROM oorder o WHERE o.district_id = :districtId AND o.fulfilled = false ORDER BY o.entrydate ASC LIMIT 1",
       nativeQuery = true)
   Optional<OrderEntity> findOldestUnfulfilledOrderOfDistrict(Long districtId);
 
   @Query(
       value =
-          "SELECT * FROM OrderEntity o WHERE o.district_id = :districtId ORDER BY entry_date DESC LIMIT 20",
+          "SELECT * FROM oorder o WHERE o.district_id = :districtId ORDER BY o.entrydate DESC LIMIT 20",
       nativeQuery = true)
   List<OrderEntity> find20MostRecentOrdersOfDistrict(Long districtId);
 }
