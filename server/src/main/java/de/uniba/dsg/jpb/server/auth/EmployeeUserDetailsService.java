2728ae7e2a83d73c66fb9f00f7db9bbe29f40799
Benedikt Full <74531112+fullben@users.noreply.github.com>
2021-08-18 17:26:36 +0200

    Make UserDetailsService explicitely JPA-specific

--- server/src/main/java/de/uniba/dsg/jpb/server/auth/EmployeeUserDetailsService.java
+++ server/src/main/java/de/uniba/dsg/jpb/server/auth/EmployeeUserDetailsService.java
@@ -1,34 +1,13 @@
 package de.uniba.dsg.jpb.server.auth;
 
-import de.uniba.dsg.jpb.server.data.access.jpa.EmployeeRepository;
-import de.uniba.dsg.jpb.server.data.model.jpa.EmployeeEntity;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 
-@Service
-public class EmployeeUserDetailsService implements UserDetailsService {
+public abstract class EmployeeUserDetailsService implements UserDetailsService {
 
-  private final EmployeeRepository employeeRepository;
-
-  @Autowired
-  public EmployeeUserDetailsService(EmployeeRepository employeeRepository) {
-    this.employeeRepository = employeeRepository;
-  }
+  EmployeeUserDetailsService() {}
 
   @Override
-  public EmployeeUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    EmployeeEntity employee = employeeRepository.findByUsername(username);
-    if (employee == null) {
-      throw new UsernameNotFoundException("Unable to find user with name " + username);
-    }
-    return new EmployeeUserDetails(
-        employee.getUsername(),
-        employee.getPasswordHash(),
-        employee.getSalt(),
-        List.of(new SimpleGrantedAuthority(Role.TERMINAL_USER.prefixedName())));
-  }
+  public abstract EmployeeUserDetails loadUserByUsername(String username)
+      throws UsernameNotFoundException;
 }
---
+++ server/src/main/java/de/uniba/dsg/jpb/server/auth/JpaEmployeeUserDetailsService.java
@@ -0,0 +1,35 @@
+package de.uniba.dsg.jpb.server.auth;
+
+import de.uniba.dsg.jpb.server.data.access.jpa.EmployeeRepository;
+import de.uniba.dsg.jpb.server.data.model.jpa.EmployeeEntity;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@ConditionalOnProperty(name = "jpb.persistence.mode", havingValue = "jpa")
+public class JpaEmployeeUserDetailsService extends EmployeeUserDetailsService {
+
+  private final EmployeeRepository employeeRepository;
+
+  @Autowired
+  public JpaEmployeeUserDetailsService(EmployeeRepository employeeRepository) {
+    this.employeeRepository = employeeRepository;
+  }
+
+  @Override
+  public EmployeeUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    EmployeeEntity employee = employeeRepository.findByUsername(username);
+    if (employee == null) {
+      throw new UsernameNotFoundException("Unable to find user with name " + username);
+    }
+    return new EmployeeUserDetails(
+        employee.getUsername(),
+        employee.getPasswordHash(),
+        employee.getSalt(),
+        List.of(new SimpleGrantedAuthority(Role.TERMINAL_USER.prefixedName())));
+  }
+}
