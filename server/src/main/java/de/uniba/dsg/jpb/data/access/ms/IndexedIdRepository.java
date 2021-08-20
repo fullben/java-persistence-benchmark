e9af11b7d6255c88ace14a1e2c7790bd1253d5dd
Benedikt Full <74531112+fullben@users.noreply.github.com>
2021-08-20 21:45:36 +0200

    Make MS base repo threadsafe

--- server/src/main/java/de/uniba/dsg/jpb/data/access/ms/IndexedIdRepository.java
+++ server/src/main/java/de/uniba/dsg/jpb/data/access/ms/IndexedIdRepository.java
@@ -6,59 +6,72 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.function.Supplier;
 import one.microstream.storage.embedded.types.EmbeddedStorageManager;
 
 public abstract class IndexedIdRepository<T extends Identifiable<I>, I> {
 
-  private transient EmbeddedStorageManager storageManager;
   private final Map<I, T> idToItem;
+  private final transient ReentrantReadWriteLock lock;
+  private transient EmbeddedStorageManager storageManager;
 
   IndexedIdRepository(EmbeddedStorageManager storageManager) {
     this.storageManager = storageManager;
+    lock = new ReentrantReadWriteLock();
     idToItem = new HashMap<>();
   }
 
   IndexedIdRepository() {
-    storageManager = null;
     idToItem = new HashMap<>();
+    lock = new ReentrantReadWriteLock();
+    storageManager = null;
   }
 
   public T findById(I id) {
-    return idToItem.get(id);
+    return read(() -> idToItem.get(id));
   }
 
   public List<T> findAll() {
-    return new ArrayList<>(idToItem.values());
+    return read(() -> new ArrayList<>(idToItem.values()));
   }
 
   public T save(T item) {
-    T res = idToItem.put(item.getId(), item);
-    persist();
-    return res;
+    return write(
+        () -> {
+          T res = idToItem.put(item.getId(), item);
+          persist();
+          return res;
+        });
   }
 
   public List<T> saveAll(Collection<T> items) {
-    for (T item : items) {
-      idToItem.put(item.getId(), item);
-    }
-    persist();
-    return new ArrayList<>(items);
+    return write(
+        () -> {
+          for (T item : items) {
+            idToItem.put(item.getId(), item);
+          }
+          persist();
+          return new ArrayList<>(items);
+        });
   }
 
   public void delete(T item) {
-    idToItem.remove(item.getId());
-    persist();
+    write(
+        () -> {
+          idToItem.remove(item.getId());
+          persist();
+        });
   }
 
   public void deleteAll(Collection<T> items) {
-    for (T item : items) {
-      idToItem.remove(item.getId());
-    }
-    persist();
-  }
-
-  private void persist() {
-    storageManager.storeAll(idToItem);
+    write(
+        () -> {
+          for (T item : items) {
+            idToItem.remove(item.getId());
+          }
+          persist();
+        });
   }
 
   void setStorageManager(EmbeddedStorageManager storageManager) {
@@ -67,5 +80,45 @@ public abstract class IndexedIdRepository<T extends Identifiable<I>, I> {
 
   EmbeddedStorageManager getStorageManager() {
     return storageManager;
+  }
+
+  private void persist() {
+    storageManager.storeAll(idToItem);
+  }
+
+  private <S> S read(Supplier<S> operation) {
+    lock.readLock().lock();
+    try {
+      return operation.get();
+    } finally {
+      lock.readLock().unlock();
+    }
+  }
+
+  private void read(Runnable operation) {
+    lock.readLock().lock();
+    try {
+      operation.run();
+    } finally {
+      lock.readLock().unlock();
+    }
+  }
+
+  private <S> S write(Supplier<S> operation) {
+    lock.writeLock().lock();
+    try {
+      return operation.get();
+    } finally {
+      lock.writeLock().unlock();
+    }
+  }
+
+  private void write(Runnable operation) {
+    lock.writeLock().lock();
+    try {
+      operation.run();
+    } finally {
+      lock.writeLock().unlock();
+    }
   }
 }
