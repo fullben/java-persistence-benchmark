4f38955557d0f3fcb6f837d0e1b11846e6379d98
Benedikt Full <74531112+fullben@users.noreply.github.com>
2021-11-21 15:02:01 +0100

    Improve stopwatch implementation and doc

--- wss-commons/src/main/java/de/uniba/dsg/wss/commons/Stopwatch.java
+++ wss-commons/src/main/java/de/uniba/dsg/wss/commons/Stopwatch.java
@@ -6,12 +6,27 @@ import java.util.concurrent.TimeUnit;
 /**
  * A very basic stopwatch implementation based on {@link System#nanoTime()}.
  *
+ * <p>Stopwatches are primarily meant for measuring the execution time of tasks. They also provide
+ * the facility for returning the measured time in a loggable format, see {@link #getDuration()}.
+ *
+ * <p>The following snippet illustrates a typical use case for the stopwatch:
+ *
+ * <pre>
+ *   Stopwatch stopwatch = new Stopwatch();
+ *   stopwatch.start();
+ *   // Do some resource/time-intensive task here
+ *   ...
+ *   stopwatch.stop();
+ *   LOG.info("Task took {}", stopwatch.getDuration());
+ * </pre>
+ *
  * @author Benedikt Full
  */
 public class Stopwatch {
 
   private long start;
   private long stop;
+  private boolean stopped;
 
   public Stopwatch() {
     this(false);
@@ -24,33 +39,105 @@ public class Stopwatch {
     }
   }
 
+  /**
+   * Starts or restarts time measurement.
+   *
+   * @see #stop()
+   */
   public void start() {
     start = System.nanoTime();
+    stop = 0;
+    stopped = false;
   }
 
+  /**
+   * Stops the time measuring and returns the elapsed time in nanoseconds.
+   *
+   * <p>Elapsed time is the amount of time that has passed since stopwatch initialization or the
+   * most recent call to {@link #start()}.
+   *
+   * @return the elapsed duration in nanoseconds
+   */
   public long stop() {
+    if (stopped) {
+      return getDurationNanos();
+    }
     stop = System.nanoTime();
+    stopped = true;
     return getDurationNanos();
   }
 
-  public void reset() {
+  /** Resets the internal state of the stopwatch to stopped. */
+  private void reset() {
     start = 0;
     stop = 0;
+    stopped = true;
   }
 
+  /**
+   * Returns the amount of nanoseconds that have been measured during the most recent measurement
+   * interval.
+   *
+   * @return the elapsed duration in nanoseconds
+   * @see #getDurationMillis()
+   * @see #getDurationSeconds()
+   * @see #getDuration()
+   */
   public long getDurationNanos() {
+    requireStopped();
     return stop - start;
   }
 
+  /**
+   * Returns the amount of milliseconds that have been measured during the most recent measurement
+   * interval.
+   *
+   * @return the elapsed duration in milliseconds
+   * @see #getDurationNanos()
+   * @see #getDurationSeconds()
+   * @see #getDuration()
+   */
   public long getDurationMillis() {
+    requireStopped();
     return TimeUnit.NANOSECONDS.toMillis(getDurationNanos());
   }
 
+  /**
+   * Returns the amount of seconds that have been measured during the most recent measurement
+   * interval.
+   *
+   * @return the elapsed duration in seconds
+   * @see #getDurationNanos()
+   * @see #getDurationMillis()
+   * @see #getDuration()
+   */
   public long getDurationSeconds() {
+    requireStopped();
     return TimeUnit.NANOSECONDS.toSeconds(getDurationNanos());
   }
 
+  /**
+   * Returns the amount of time that has measured during the most recent measurement interval in a
+   * human-readable format. Examples:
+   *
+   * <ul>
+   *   <li>68 nanoseconds: {@code "68 ns"}
+   *   <li>23 milliseconds: {@code "23 ms"}
+   *   <li>1032 milliseconds: {@code "1.032 seconds"}
+   *   <li>1840 seconds: {@code "30 minutes 40 seconds"}
+   * </ul>
+   *
+   * <p>Note that the current implementation is only meant for returning human-readable
+   * representations of the elapsed time in nanoseconds, milliseconds, seconds, and minutes. Hours,
+   * days, or even larger units of time are not used.
+   *
+   * @return the elapsed duration as a human-readable string
+   * @see #getDurationNanos()
+   * @see #getDurationMillis()
+   * @see #getDurationSeconds()
+   */
   public String getDuration() {
+    requireStopped();
     Duration duration = Duration.ofNanos(getDurationNanos());
     int minutesPart = duration.toMinutesPart();
     if (minutesPart > 0) {
@@ -62,7 +149,8 @@ public class Stopwatch {
       if (secs > 0) {
         int millisPart = duration.toMillisPart();
         if (millisPart > 0) {
-          return toSecondsAndMillisString(duration.toSeconds(), duration.toMillisPart()) + " seconds";
+          return toSecondsAndMillisString(duration.toSeconds(), duration.toMillisPart())
+              + " seconds";
         }
         return duration.toSeconds() + " seconds";
       } else {
@@ -86,6 +174,12 @@ public class Stopwatch {
       return seconds + ".0" + millis;
     } else {
       return seconds + "." + millis;
+    }
+  }
+
+  private void requireStopped() {
+    if (!stopped) {
+      throw new IllegalStateException("Stopwatch is currently running");
     }
   }
 }
