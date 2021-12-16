a90329ec69520f94275473f227f81de66f092f4a
Benedikt Full <74531112+fullben@users.noreply.github.com>
2021-12-16 15:51:44 +0100

    Add doc for the resource controller methods

--- wss-server-core/src/main/java/de/uniba/dsg/wss/api/ResourceController.java
+++ wss-server-core/src/main/java/de/uniba/dsg/wss/api/ResourceController.java
@@ -8,6 +8,7 @@ import de.uniba.dsg.wss.data.transfer.representations.OrderRepresentation;
 import de.uniba.dsg.wss.data.transfer.representations.ProductRepresentation;
 import de.uniba.dsg.wss.data.transfer.representations.StockRepresentation;
 import de.uniba.dsg.wss.data.transfer.representations.WarehouseRepresentation;
+import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import javax.validation.constraints.NotBlank;
 import org.springframework.http.MediaType;
@@ -28,38 +29,46 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public interface ResourceController {
 
   @GetMapping(value = "products", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  @Operation(summary = "Returns all products")
   ResponseEntity<Iterable<ProductRepresentation>> getProducts();
 
   @GetMapping(value = "employees/{username}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  @Operation(summary = "Returns the employee identified by the given username")
   ResponseEntity<EmployeeRepresentation> getEmployee(@NotBlank @PathVariable String username);
 
   @GetMapping(value = "warehouses", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  @Operation(summary = "Returns all warehouses")
   ResponseEntity<List<WarehouseRepresentation>> getWarehouses();
 
   @GetMapping(
       value = "warehouses/{warehouseId}/districts",
       produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  @Operation(summary = "Returns the 10 districts of the specified warehouse")
   ResponseEntity<List<DistrictRepresentation>> getWarehouseDistricts(
       @NotBlank @PathVariable String warehouseId);
 
   @GetMapping(
       value = "warehouses/{warehouseId}/stocks",
       produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  @Operation(summary = "Returns the stocks of the specified warehouse")
   ResponseEntity<List<StockRepresentation>> getWarehouseStocks(
       @NotBlank @PathVariable String warehouseId);
 
   @GetMapping(
       value = "warehouses/{warehouseId}/districts/{districtId}/customers",
       produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  @Operation(summary = "Returns the customers of the specified district")
   ResponseEntity<List<CustomerRepresentation>> getDistrictCustomers(
       @NotBlank @PathVariable String warehouseId, @NotBlank @PathVariable String districtId);
 
   @GetMapping(
       value = "warehouses/{warehouseId}/districts/{districtId}/orders",
       produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  @Operation(summary = "Returns the orders of the specified district")
   ResponseEntity<List<OrderRepresentation>> getDistrictOrders(
       @NotBlank @PathVariable String warehouseId, @NotBlank @PathVariable String districtId);
 
   @GetMapping(value = "carriers", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  @Operation(summary = "Returns all carriers")
   ResponseEntity<List<CarrierRepresentation>> getCarriers();
 }
